e9c4406fa7012d4bbfa9746e255c7bdddacda975
ClausKlein <claus.klein@arcormail.de>
2023-05-19 19:26:58 +0200

    Use fmt lib if std::format is not supported

--- modules/mod1/mod1io.cpp
+++ modules/mod1/mod1io.cpp
@@ -13,7 +13,14 @@
 module;              // start module unit with global module fragment
 
 #include <iostream>
+
+#ifdef __cpp_lib_format
 #include <format>
+using std::format;
+#else
+#include <fmt/format.h>
+using fmt::format;
+#endif
 
 module Mod1;         // implementation unit of module Mod1
 
@@ -23,11 +30,11 @@ void Customer::print() const
   std::cout << name << ":\n";
   // print order entries:
   for (const auto& od : orders) {
-    std::cout << std::format("{:3} {:14} {:6.2f} {:6.2f}\n",
+    std::cout << format("{:3} {:14} {:6.2f} {:6.2f}\n",
                              od.count, od.name, od.price, od.count * od.price);
   }
   // print sum:
-  std::cout << std::format("{:25} ------\n", ' ');
-  std::cout << std::format("{:25} {:6.2f}\n", "    Sum:", sumPrice());
+  std::cout << format("{:25} ------\n", ' ');
+  std::cout << format("{:25} {:6.2f}\n", "    Sum:", sumPrice());
 }
 
